52ecba94f7e44e0d30ac1179396861f89623d7e1
Owais Hashim <owaishashim41@gmail.com>
2026-02-26 15:58:51 +0500

    init files for modules

---
+++ backend/app/services/__init__.py
@@ -0,0 +1,13 @@
+from app.services import github_service
+from app.services import code_parser
+from app.services import ai_review
+from app.services import scoring
+from app.services import notification
+
+__all__ = [
+    "github_service",
+    "code_parser",
+    "ai_review",
+    "scoring",
+    "notification",
+]
